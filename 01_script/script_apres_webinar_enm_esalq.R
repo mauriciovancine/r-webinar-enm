bdcf4eeb9488bfb1fae5fa44e392bdf4bd662643
mauriciovancine <mauricio.vancine@gmail.com>
2020-07-09 01:33:56 -0300

    2020-07-08

--- 01_script/script_apres_webinar_enm_esalq.R
+++ 01_script/script_apres_webinar_enm_esalq.R
@@ -1,5 +1,5 @@
 #' ---
-#' title: enm introduction
+#' title: webinar intro enm
 #' author: mauricio vancine
 #' date: 2020-07-16
 #' ---
@@ -9,15 +9,15 @@
 rm(list = ls())
 
 # packages
-if(!require(here)) install.packages("here")
 if(!require(tidyverse)) install.packages("tidyverse")
 if(!require(spocc)) install.packages("spocc")
 if(!require(CoordinateCleaner)) install.packages("CoordinateCleaner")
 if(!require(rnaturalearth)) install.packages("rnaturalearth")
 if(!require(rvest)) install.packages("rvest")
-if(!require(ggsn)) install.packages("ggsn")
+if(!require(ggspatial)) install.packages("ggspatial")
 if(!require(raster)) install.packages("raster")
 if(!require(RColorBrewer)) install.packages("RColorBrewer")
+if(!require(GGally)) install.packages("GGally")
 if(!require(sf)) install.packages("sf")
 if(!require(tmap)) install.packages("tmap")
 if(!require(wesanderson)) devtools::install_github("karthik/wesanderson")
@@ -26,28 +26,23 @@ if(!require(ecospat)) install.packages("ecospat")
 if(!require(dismo)) install.packages("dismo")
 if(!require(kernlab)) install.packages("e1071")
 if(!require(randomForest)) install.packages("randomForest")
-if(!require(rJava)) install.packages("rJava") # install java - https://www.java.com/pt_BR/download/
 
+# install java - windows - https://www.java.com/pt_BR/download/
+# install java - ubuntu - sudo apt install -y default-jre default-jdk && sudo R CMD javareconf ## rjava
+if(!require(rJava)) install.packages("rJava") 
 
 # 1. occurrences ----------------------------------------------------------
-# directory
-setwd(here::here())
-dir.create("01_occurrences"); setwd("01_occurrences")
-path <- getwd()
-path
-
 # import limit ----
-# data
 li <- rnaturalearth::countries110 %>% 
   sf::st_as_sf()
 li
 
+tm_shape(li) +
+  tm_polygons()
+
 # download ----
 # species
 sp <- "Chrysocyon brachyurus"
-
-# directory
-setwd(path); dir.create("01_raw"); setwd("01_raw")
 
 # occ
 occ_spocc <- spocc::occ(query = sp, 
@@ -80,9 +75,6 @@ tm_shape(li, bbox = occ_data_vector) +
   tm_layout(legend.text.fontface = "italic")
 
 # date filter ----
-# directory
-setwd(path); dir.create("02_clean"); setwd("02_clean")
-
 # verify
 occ_data$year %>% table(useNA = "always")
 
@@ -108,7 +100,6 @@ occ_data_date %>%
   theme(legend.text = element_text(face = "italic"),
         legend.position = "none",
         strip.text = element_text(size = 10, face = "italic"))
-ggsave(filename = "occ_plot_date.png", wi = 20, he = 15, un = "cm", dpi = 300)
 
 # map
 occ_data_date_vector <- occ_data_date %>% 
@@ -135,7 +126,6 @@ flags_bias <- CoordinateCleaner::clean_coordinates(
   species = "species",
   lon = "longitude", 
   lat = "latitude",
-  outliers_mtp = 2,
   tests = c("capitals", # radius around capitals
             "centroids", # radius around country and province centroids
             "duplicates", # records from one species with identical coordinates
@@ -177,35 +167,59 @@ tm_shape(li, bbox = occ_data_date_bias_vector) +
   tm_graticules(lines = FALSE) +
   tm_layout(legend.text.fontface = "italic")
 
-# spatial ----
+# spatial limit ----
+# spatial occ
+occ_data_date_bias_vector <- occ_data_date_bias %>%
+  dplyr::mutate(x = longitude, lon = longitude, y = latitude, lat = latitude) %>% 
+  sf::st_as_sf(coords = c("lon", "lat"), crs = 4326)
+occ_data_date_bias_vector
+
+# extent
+li_sa <- rnaturalearth::ne_countries(scale = 110, continent = "South America", returnclass = "sf")
+li_sa %>% tm_shape() + tm_polygons() + tm_graticules(lines = FALSE)
+
+# crop to limit
+occ_data_date_bias_limit <- sf::st_crop(occ_data_date_bias_vector, li_sa) 
+occ_data_date_bias_limit
+
+# map
+tm_shape(li_sa) +
+  tm_polygons() +
+  tm_shape(occ_data_date_bias_limit) +
+  tm_dots(size = .2, shape = 21, col = "species",  
+          palette = "Set1", title = "Species") +
+  tm_graticules(lines = FALSE) +
+  tm_layout(legend.text.fontface = "italic")
+
+# spatial oppc ----
 # prepare
-occ_data_date_bias <- occ_data_date_bias %>%
+occ_data_date_bias_limit <- occ_data_date_bias_limit %>%
   dplyr::mutate(x = longitude, y = latitude) %>% 
   as.data.frame
-occ_data_date_bias
+occ_data_date_bias_limit
 
 # desaggregation
-occ_data_date_bias_spatial <- ecospat::ecospat.occ.desaggregation(
-  xy = occ_data_date_bias,
+occ_data_date_bias_limit_spatial <- ecospat::ecospat.occ.desaggregation(
+  xy = occ_data_date_bias_limit,
   min.dist = .5, 
   by = "species") %>%
   tibble::as_tibble() %>% 
   dplyr::select(-x, -y)
-occ_data_date_bias_spatial
+occ_data_date_bias_limit_spatial
 
 # verify
-occ_data_date_bias %>% dplyr::count(species)
-occ_data_date_bias_spatial %>% dplyr::count(species)
+occ_data_date_bias_limit %>% dplyr::count(species)
+occ_data_date_bias_limit_spatial %>% dplyr::count(species)
 
 # map
-occ_data_date_bias_spatial_vector <- occ_data_date_bias_spatial %>%
+occ_data_date_bias_limit_spatial_vector <- occ_data_date_bias_limit_spatial %>%
   dplyr::mutate(x = longitude, lon = longitude, y = latitude, lat = latitude) %>% 
   sf::st_as_sf(coords = c("lon", "lat"), crs = 4326)
-occ_data_date_bias_spatial_vector
+occ_data_date_bias_limit_spatial_vector
 
-tm_shape(li, bbox = li_sa) +
+tm_shape(li_sa) +
   tm_polygons() +
-  tm_shape(occ_data_date_bias_spatial_vector) +
+  tm_shape(occ_data_date_bias_limit_spatial_vector) +
   tm_dots(size = .2, shape = 21, col = "species",  
           palette = "Set1", title = "Species") +
   tm_graticules(lines = FALSE) +
@@ -216,48 +230,11 @@ tm_shape(li, bbox = li_sa) +
 occ_data %>% dplyr::count(species)
 occ_data_date %>% dplyr::count(species)
 occ_data_date_bias %>% dplyr::count(species)
-occ_data_date_bias_spatial %>% dplyr::count(species)
-
-# export ----
-readr::write_csv(occ_data_date_bias_spatial, 
-                 paste0("occ_clean_date_bias_spatial.csv"))
+occ_data_date_bias_limit %>% dplyr::count(species)
+occ_data_date_bias_limit_spatial %>% dplyr::count(species)
 
 # 2. variables ------------------------------------------------------------
-# directory
-setwd(here::here())
-dir.create("02_variables"); setwd("02_variables")
-path <- getwd()
-path
-
-# limits -----
-li <- rnaturalearth::ne_countries(scale = 110, continent = "South America", returnclass = "sf")
-li
-
-li_ex <- li %>% 
-  sf::st_bbox() %>% 
-  sf::st_as_sfc()
-li_ex
-
-# plot
-tm_shape(li_ex) +
-  tm_polygons(border.col = "red") +
-  tm_shape(li) +
-  tm_polygons(col = "gray70")
-
-# directory
-dir.create("00_limit"); setwd("00_limit")
-getwd()
-
-# export
-sf::write_sf(li, "limit.shp")
-sf::write_sf(li_ex, "limit_ext.shp")
-
 # download variables ----
-# directory
-setwd(path); dir.create("01_raw"); setwd("01_raw")
-getwd()
-
-# download
 download.file(url = "https://biogeo.ucdavis.edu/data/worldclim/v2.1/base/wc2.1_10m_bio.zip",
               destfile = "wc2.1_10m_bio.zip", mode = "wb")
 
@@ -280,12 +257,9 @@ var
 plot(var$bio01)
 
 # extent and resolution ----
-# directory
-setwd(path); dir.create("02_processed"); setwd("02_processed")
-
 # adjust extent and resolution
-var_li <- raster::crop(x = var, y = li) %>% 
-  raster::mask(li) %>% 
+var_li <- raster::crop(x = var, y = li_sa) %>% 
+  raster::mask(li_sa) %>% 
   raster::aggregate(., fact = .5/res(.)[1])
 var_li
 
@@ -296,11 +270,11 @@ tm_shape(var_li$bio01) +
   tm_borders(col = "black") +
   tm_layout(legend.position = c("right", "bottom"))
 
-# correlation ----
-# directory
-setwd(path); dir.create("03_correlation"); setwd("03_correlation")
-getwd()
+# delete
+unlink(dir(pattern = ".zip$"))
+unlink(dir(pattern = ".tif$"))
 
+# correlation ----
 # extract values
 var_da <- var_li %>% 
   raster::values() %>% 
@@ -308,24 +282,11 @@ var_da <- var_li %>%
   tidyr::drop_na()
 var_da
 
-# verify
-tibble::glimpse(var_da)
-
 # correlation
 cor_table <- corrr::correlate(var_da, method = "spearman") 
 cor_table
 
-# prepare table
-cor_table_summary <- cor_table %>% 
-  corrr::shave() %>%
-  corrr::fashion()
-cor_table_summary
-
-# export
-readr::write_csv(cor_table_summary, "var_table_correlacao.csv")
-
-# select variables
-# correlated variables
+# select variables - correlated variables
 fi_07 <- cor_table %>% 
   corrr::as_matrix() %>% 
   caret::findCorrelation(cutoff = .7, names = TRUE, verbose = TRUE)
@@ -355,21 +316,6 @@ var_ggpairs <- var_da_cor07 %>%
           axisLabels = "none") +
   theme_bw()
 var_ggpairs
-ggsave(filename = "var_plot_correlation.png", plot = var_ggpairs, wi = 20, he = 15, un = "cm", dpi = 300)
-
-# create variables -----
-# directory
-setwd(path); dir.create("04_processed_correlation"); setwd("04_processed_correlation")
-getwd()
-
-# export
-raster::writeRaster(x = raster::subset(var_li, colnames(var_da_cor07)), 
-                    filename = paste0("var_wc21_55km_", colnames(var_da_cor07)), 
-                    bylayer = TRUE, 
-                    options = c("COMPRESS=DEFLATE"), 
-                    format = "GTiff",
-                    progress = "text",
-                    overwrite = TRUE)
 
 # 3. enm ------------------------------------------------------------------
 # maxent
@@ -382,21 +328,20 @@ if(file.exists(paste0(system.file(package = "dismo"), "/java/maxent.jar"))){
                 "maxent.zip", mode = "wb")
   unzip("maxent.zip")}
 
-# directory
-setwd(here::here())
-
-# directory
-setwd(path); dir.create("03_enms"); setwd("03_enms")
-
 # data ----
-occ <- occ_data_date_bias_spatial
+occ <- occ_data_date_bias_limit_spatial
 occ
 
-var <- var_li
+var <- raster::subset(var_li, colnames(var_da_cor07))
 var
 
-plot(var[[1]])
-points(occ[, 1], occ[, 2], pch = 20)
+tm_shape(var$bio02) +
+  tm_raster(pal = "-Spectral", n = 10) +
+  tm_shape(li) +
+  tm_borders(col = "black") +
+  tm_shape(occ %>% sf::st_as_sf(coords = c("longitude", "latitude"), crs = 4326)) +
+  tm_bubbles(size = .1, col = "blue") +
+  tm_layout(legend.position = c("right", "bottom"))
 
 # selecting presence and pseudo-absence data ----
 pr_specie <- occ %>% 
@@ -410,8 +355,17 @@ pa_specie <- dismo::randomPoints(mask = var, n = nrow(pr_specie)) %>%
   dplyr::mutate(id = seq(nrow(.)))
 pa_specie
 
-bkg <- dismo::randomPoints(mask = var, n = 1e5, warn = FALSE)
+bkg <- dismo::randomPoints(mask = var, n = 1e3, warn = FALSE)
 bkg
+
+tm_shape(li) +
+  tm_borders(col = "black") +
+  tm_shape(bkg %>% tibble::as_tibble() %>% sf::st_as_sf(coords = c("x", "y"), crs = 4326)) +
+  tm_bubbles(size = .1, col = "gray") +
+  tm_shape(pr_specie %>% sf::st_as_sf(coords = c("longitude", "latitude"), crs = 4326)) +
+  tm_bubbles(size = .1, col = "blue") +
+  tm_shape(pa_specie %>% sf::st_as_sf(coords = c("longitude", "latitude"), crs = 4326)) +
+  tm_bubbles(size = .1, col = "red")
 
 # partitioning data	----
 pr_sample_train <- pr_specie %>% 
@@ -436,6 +390,7 @@ train_pb <- dismo::prepareData(x = var,
                                p = pr_specie %>% dplyr::filter(id %in% pr_sample_train) %>% dplyr::select(longitude, latitude), 
                                b = bkg) %>% na.omit
 train_pb
+train_pb %>% nrow()
 
 test <- dismo::prepareData(x = var, 
                            p = pr_specie %>% dplyr::filter(!id %in% pr_sample_train) %>% dplyr::select(longitude, latitude), 
@@ -445,6 +400,7 @@ test
 # model fitting ----
 # presence-only - envelope
 BIO <- dismo::bioclim(x = train_pa %>% dplyr::filter(pb == 1) %>% dplyr::select(-pb))
+plot(BIO)
 
 # presence-only - distance-based
 DOM <- dismo::domain(x = train_pa %>% dplyr::filter(pb == 1) %>% dplyr::select(-pb))
@@ -456,44 +412,115 @@ SVM <- e1071::svm(formula = pb ~ ., data = train_pa)
 # presence-background
 Sys.setenv(NOAWT = TRUE)
 MAX <- dismo::maxent(x = train_pb %>% dplyr::select(-pb), p = train_pb %>% dplyr::select(pb))
+MAX
 
 # prediction ----
 model_predict_bio <- raster::predict(var, BIO, progress = "text")
 model_predict_bio
+plot(model_predict_bio)
 
 model_predict_dom <- raster::predict(var, DOM, progress = "text")
 model_predict_dom
+plot(model_predict_dom)
 
 model_predict_rfr <- raster::predict(var, RFR, progress = "text")
 model_predict_rfr
+plot(model_predict_rfr)
 
 model_predict_svm <- raster::predict(var, SVM, progress = "text")
 model_predict_svm
+plot(model_predict_svm)
 
 model_predict_max <- raster::predict(var, MAX, progress = "text")
 model_predict_max
+plot(model_predict_max)
 
 # 4. evaluation -----------------------------------------------------------
-eval <- dismo::evaluate(p = test %>% dplyr::filter(pb == 1) %>% dplyr::select(-pb), 
-                        a = test %>% dplyr::filter(pb == 0) %>% dplyr::select(-pb), 
-                        model = fit[[a]])
+# eval bioclim
+eval_BIO <- dismo::evaluate(p = test %>% dplyr::filter(pb == 1) %>% dplyr::select(-pb), 
+                            a = test %>% dplyr::filter(pb == 0) %>% dplyr::select(-pb), 
+                            model = BIO)
+eval_BIO
+plot(eval_BIO, "ROC")
 
-# indices
-id_eval_spec_sens <- which(eval@t == dismo::threshold(eval, "spec_sens"))
-tss_spec_sens <- eval@TPR[id_eval_spec_sens] + eval@TNR[id_eval_spec_sens] - 1
+id_eval_spec_sens_BIO <- which(eval_BIO@t == dismo::threshold(eval_BIO, "spec_sens"))
+tss_spec_sens_BIO <- eval_BIO@TPR[id_eval_spec_sens_BIO] + eval_BIO@TNR[id_eval_spec_sens_BIO] - 1
+tss_spec_sens_BIO
 
-# 5. maps -----------------------------------------------------------------
+# eval domain
+eval_DOM <- dismo::evaluate(p = test %>% dplyr::filter(pb == 1) %>% dplyr::select(-pb), 
+                            a = test %>% dplyr::filter(pb == 0) %>% dplyr::select(-pb), 
+                            model = DOM)
+eval_DOM
+plot(eval_DOM, "ROC")
+
+id_eval_spec_sens_DOM <- which(eval_DOM@t == dismo::threshold(eval_DOM, "spec_sens"))
+tss_spec_sens_DOM <- eval_DOM@TPR[id_eval_spec_sens_DOM] + eval_DOM@TNR[id_eval_spec_sens_DOM] - 1
+tss_spec_sens_DOM
+
+# eval random forest
+eval_RFR <- dismo::evaluate(p = test %>% dplyr::filter(pb == 1) %>% dplyr::select(-pb), 
+                            a = test %>% dplyr::filter(pb == 0) %>% dplyr::select(-pb), 
+                            model = RFR)
+eval_RFR
+plot(eval_RFR, "ROC")
+
+id_eval_spec_sens_RFR <- which(eval_RFR@t == dismo::threshold(eval_RFR, "spec_sens"))
+tss_spec_sens_RFR <- eval_RFR@TPR[id_eval_spec_sens_RFR] + eval_RFR@TNR[id_eval_spec_sens_RFR] - 1
+tss_spec_sens_RFR
+
+# eval svm
+eval_SVM <- dismo::evaluate(p = test %>% dplyr::filter(pb == 1) %>% dplyr::select(-pb), 
+                            a = test %>% dplyr::filter(pb == 0) %>% dplyr::select(-pb), 
+                            model = SVM)
+eval_SVM
+plot(eval_SVM, "ROC")
+
+id_eval_spec_sens_SVM <- which(eval_SVM@t == dismo::threshold(eval_SVM, "spec_sens"))
+tss_spec_sens_SVM <- eval_SVM@TPR[id_eval_spec_sens_SVM] + eval_SVM@TNR[id_eval_spec_sens_SVM] - 1
+tss_spec_sens_SVM
+
+# eval maxent
+eval_MAX <- dismo::evaluate(p = test %>% dplyr::filter(pb == 1) %>% dplyr::select(-pb), 
+                            a = test %>% dplyr::filter(pb == 0) %>% dplyr::select(-pb), 
+                            model = MAX)
+eval_MAX
+plot(eval_MAX, "ROC")
+
+id_eval_spec_sens_MAX <- which(eval_MAX@t == dismo::threshold(eval_MAX, "spec_sens"))
+tss_spec_sens_MAX <- eval_MAX@TPR[id_eval_spec_sens_MAX] + eval_MAX@TNR[id_eval_spec_sens_MAX] - 1
+tss_spec_sens_MAX
+
+# 5. ensembles ------------------------------------------------------------
+# models
+models <- raster::stack(model_predict_bio, model_predict_dom, model_predict_rfr, 
+                        model_predict_svm, model_predict_max)
+models
+
+# mean
+ens_mean <- mean(models)
+ens_mean
+plot(ens_mean)
+
+# weighted.mean
+aucs <- c(eval_BIO@auc, eval_DOM@auc, eval_RFR@auc, eval_SVM@auc, eval_MAX@auc)
+w <- (aucs-0.5)^2 # retirar 0.5 (aleatorio) e eleva ao quadrado para valores muito altos
+ens_wei_mean <- weighted.mean(models, w)
+plot(ens_wei_mean)
+
+# 6. maps -----------------------------------------------------------------
 # data
-da <- raster::rasterToPoints(ens) %>% 
+da <- raster::rasterToPoints(ens_wei_mean) %>% 
   tibble::as_tibble() %>% 
-  dplyr::rename(sui = names(ens))
+  dplyr::rename(sui = layer)
+da
 
 map_sui <- ggplot() +
   geom_raster(data = da, aes(x, y, fill = sui)) +
   geom_sf(data = li, fill = NA, color = "gray30") +
+  geom_point(data = occ, aes(x = longitude, y = latitude)) +
   scale_color_manual(values = "black", guide = guide_legend(order = 1)) +
-  scale_fill_gradientn(colours = wesanderson::wes_palette("Zissou1", n = 100, type = "continuous"),
-                       limits = c(0, 1)) +
+  scale_fill_gradientn(colours = wesanderson::wes_palette("Zissou1", n = 100, type = "continuous")) +
   coord_sf(xlim = sf::st_bbox(li)[c(1, 3)], ylim = sf::st_bbox(li)[c(2, 4)]) +
   labs(x = "Longitude", y = "Latitude", fill = "Suitability") +
   annotation_scale(location = "br", width_hint = .3) +
@@ -508,7 +535,43 @@ map_sui <- ggplot() +
                                          linetype = "solid", 
                                          colour = "black"),
         axis.title = element_text(size = 12, face = "plain"),
-        legend.position = c(.75, .17))
+        legend.position = c(.85, .17))
 map_sui
+
+# data potential
+# threshold
+lpt <- raster::extract(ens_wei_mean, occ[, 3:4]) %>% min
+lpt
+
+ens_wei_mean_lpt <- ens_wei_mean >= lpt
+
+da_lpt <- ens_wei_mean_lpt %>% 
+  raster::rasterToPoints() %>% 
+  tibble::as_tibble() %>% 
+  dplyr::rename(sui = layer)
+da_lpt
+
+map_sui_lpt <- ggplot() +
+  geom_raster(data = da_lpt, aes(x, y, fill = sui)) +
+  geom_sf(data = li, fill = NA, color = "gray30") +
+  geom_point(data = occ, aes(x = longitude, y = latitude)) +
+  scale_color_manual(values = "black", guide = guide_legend(order = 1)) +
+  scale_fill_gradientn(colours = wesanderson::wes_palette("Zissou1", n = 2, type = "continuous")) +
+  coord_sf(xlim = sf::st_bbox(li)[c(1, 3)], ylim = sf::st_bbox(li)[c(2, 4)]) +
+  labs(x = "Longitude", y = "Latitude", fill = "Suitability") +
+  annotation_scale(location = "br", width_hint = .3) +
+  annotation_north_arrow(location = "tr", which_north = "true", 
+                         pad_x = unit(.3, "cm"), pad_y = unit(.3, "cm"),
+                         style = north_arrow_fancy_orienteering) +
+  theme_bw() +
+  theme(legend.title = element_text(size = 12, face = "bold"),
+        legend.text = element_text(size = 10),
+        legend.background = element_rect(fill = "white",
+                                         size = 0.3, 
+                                         linetype = "solid", 
+                                         colour = "black"),
+        axis.title = element_text(size = 12, face = "plain"),
+        legend.position = c(.85, .17))
+map_sui_lpt
 
 # end ---------------------------------------------------------------------
--- README.md
+++ README.md
@@ -6,25 +6,25 @@ Data: 16-07-2020
 Horário: 10h - 12h
 
 ## Apresentação
-Google Slides: https://docs.google.com/presentation/d/12yra_kP_0U_QvTWWJgdT-u9weewWBDpH0FrDqYvOUS0/edit?usp=sharing 
+Google Slides: https://docs.google.com/presentation/d/12yra_kP_0U_QvTWWJgdT-u9weewWBDpH0FrDqYvOUS0
 <br>
 PDF: https://github.com/mauriciovancine/r-webinar-enm/blob/master/00_apresentacao/apres_webinar_enm_esalq.pdf
 
 ## Script
 Script no R: https://github.com/mauriciovancine/r-webinar-enm/blob/master/01_script/script_apres_webinar_enm_esalq.R
 
-## Pacotes à serem instalados
+## Pacotes à serem instalados - para quem quiser seguir o script
 ```r
 # packages
-if(!require(here)) install.packages("here")
 if(!require(tidyverse)) install.packages("tidyverse")
 if(!require(spocc)) install.packages("spocc")
 if(!require(CoordinateCleaner)) install.packages("CoordinateCleaner")
 if(!require(rnaturalearth)) install.packages("rnaturalearth")
 if(!require(rvest)) install.packages("rvest")
-if(!require(ggsn)) install.packages("ggsn")
+if(!require(ggspatial)) install.packages("ggspatial")
 if(!require(raster)) install.packages("raster")
 if(!require(RColorBrewer)) install.packages("RColorBrewer")
+if(!require(GGally)) install.packages("GGally")
 if(!require(sf)) install.packages("sf")
 if(!require(tmap)) install.packages("tmap")
 if(!require(wesanderson)) devtools::install_github("karthik/wesanderson")
@@ -33,5 +33,8 @@ if(!require(ecospat)) install.packages("ecospat")
 if(!require(dismo)) install.packages("dismo")
 if(!require(kernlab)) install.packages("e1071")
 if(!require(randomForest)) install.packages("randomForest")
-if(!require(rJava)) install.packages("rJava") # install java - https://www.java.com/pt_BR/download/
+
+# install java - windows - https://www.java.com/pt_BR/download/
+# install java - ubuntu - sudo apt install -y default-jre default-jdk && sudo R CMD javareconf ## rjava
+if(!require(rJava)) install.packages("rJava") 
 ```
